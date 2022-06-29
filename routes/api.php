08f460a2da20fa9ee77a8b7108da27edac53e6bc
xeemax <zeeshanahmad.appelit@nextbridge.com>
2022-06-29 17:35:21 +0500

    added passport authentication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
+    Route::post('logout', [RegisterController::class, 'logout']);
 });
