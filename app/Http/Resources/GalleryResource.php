88a17549efbc30016993889d70b582d07fa53b7f
xeemax <xeemax2.0@gmail.com>
2022-07-10 03:45:41 +0500

    added gallery resource

---
+++ app/Http/Resources/GalleryResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GalleryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'description' => $this->description,
+            'url' => $this->url,
+            'page_number' => $this->page_number,
+        ];
+    }
+}
