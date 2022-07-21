fb4df92e448e52b7b5922dc552289f52c6ec56da
xeemax <xeemax2.0@gmail.com>
2022-07-22 03:35:16 +0500

    changes in routes

--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,12 @@ use App\Http\Controllers\API\GalleryController;
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 Route::post('gallery', [GalleryController::class, 'index']);
-Route::post('products', [ProductController::class, 'index']);
+Route::resource('products', ProductController::class);
+Route::resource('gallery', GalleryController::class);
 
 Route::middleware('auth:api')->group( function () {
-    Route::resource('products', ProductController::class);
     Route::resource('categories', CategoriesController::class);
     Route::resource('orders', OrdersController::class);
-    Route::resource('gallery', GalleryController::class);
     Route::post('imageUpload',[GalleryController::class,'imageUpload']);
     Route::post('logout', [RegisterController::class, 'logout']);
     Route::get('/validate-token', function (Request $request) {return response()->json(['authenticated' => true]);});
