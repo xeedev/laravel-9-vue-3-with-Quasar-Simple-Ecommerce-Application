9947cdfe4ee9f8711ca7cafc2b96c85288011ee7
xeemax <xeemax2.0@gmail.com>
2022-07-26 07:14:19 +0500

    added order confirm and received mail

---
+++ app/Mail/NewOrder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewOrder extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $user;
+    protected $text;
+    protected $email;
+    protected $contact;
+    protected $total;
+    protected $transaction_id;
+    protected $address;
+    protected $city;
+    protected $country;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $text, $contact ,$email,$total, $transaction_id, $address, $city, $country)
+    {
+        $this->user = $user;
+        $this->text = $text;
+        $this->email = $email;
+        $this->contact = $contact;
+        $this->total = $total;
+        $this->transaction_id = $transaction_id;
+        $this->address = $address;
+        $this->city = $city;
+        $this->country = $country;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+            return $this->from('', $this->user)
+                ->subject("New Order From:- {$this->user}")->view('emails.new-order')->with([
+                            'user' => $this->user,
+                            'text' => $this->text,
+                            'email' => $this->email,
+                            'contact' => $this->contact,
+                            'total' => $this->total,
+                            'transaction_id' => $this->transaction_id,
+                            'address' => $this->address,
+                            'city' => $this->city,
+                            'country' => $this->country,
+                ]);
+    }
+}
